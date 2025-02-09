a9c5658b585fdf51a395f853a035b97cb65fa6d5
Samuliak <samuliak77@gmail.com>
2025-02-09 16:26:50 +0100

    remove some unused code

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,6 @@
 #include "horizon/horizon.hpp"
 #include "hypervisor/hypervisor.hpp"
 
-/*
-void PrintCodeAsCArray(const std::span<u32>& code, usize limit) {
-    printf("std::vector<u32> test_code = {\n");
-    for (int i = 0; i < code.size() && i < limit; i++) {
-        printf("0x%08x, ", code[i]);
-        if (i % 4 == 3) {
-            printf("\n");
-        }
-    }
-    printf("};\n");
-}
-*/
-
-/*
-std::vector<u32> test_code = {
-    0xf100001f, // cmp x0, #0x0
-    0xba411824, // ccmn       x1, #0x1, #0x4
-    0xD4000003, // smc #0
-    // 0x01, 0x00, 0x00, 0xD4, // svc #0
-    0xD4000002, // hvc #0
-    0xD4200000, // brk #0
-};
-*/
-
-/*
-std::vector<u32> test_code = {
-    0xf100001f,
-    0xba411824,
-    0x54000040,
-    0x14000bc8,
-    0xaa0003f9,
-    0xaa0103fa,
-    0xaa1e03fb,
-    0x910003fc,
-    0x10fffb00,
-    0x10000261,
-    0xD4200000, // 0x94000176
-    0x900000a9,
-    0xf9078d3c,
-    0xaa1903e0,
-    0xaa1a03e1,
-    0xaa1b03e2,
-    0x9400031c,
-    0x900000a0,
-    0xb94f3000,
-    0x900000a1,
-    0xf9479421,
-    0xf000009e,
-    0xf94267de,
-    0x14000029,
-};
-*/
-
 // HACK
 const std::string path =
     "/Users/samuliak/Documents/deko3d_examples/build/0_hello_world.nro";
@@ -67,20 +14,7 @@ const std::string path =
 //    printf("PRINT_PC_TO_ADDR: 0x%016x\n", pc - 0x80000000 + 0x80)
 
 // HACK
-void SET_INSTRUCTION(u32* data, i64 addr /*, u32 old_instruction*/,
-                     u32 new_instruction) {
-    /*
-    printf("ORIGINAL: 0x%08x\n", data[index]);
-    for (i64 i = index; i < index + 256; i++) {
-        if (data[i] == old_instruction) {
-            printf("OFFSET: %lli\n", i - index);
-            data[i] = new_instruction;
-            return;
-        }
-    }
-
-    throw;
-    */
+void SET_INSTRUCTION(u32* data, i64 addr, u32 new_instruction) {
     data[(addr - 0x80) / 4] = new_instruction;
 }
 
@@ -97,20 +31,6 @@ int main(int argc, const char* argv[]) {
     BinaryReader reader(ifs, size);
     Rom* rom = ParseNRO(reader);
     ifs.close();
-
-    // HACK
-    /*
-    std::vector<u8> test_code_bytes(test_code.size() * sizeof(u32));
-    std::memcpy(test_code_bytes.data(), test_code.data(),
-                test_code_bytes.size());
-    for (int i = 0; i < test_code_bytes.size(); i++) {
-        printf("%02X ", test_code_bytes[i]);
-        if (i % 16 == 15) {
-            printf("\n");
-        }
-    }
-    printf("\n");
-    */
 
     // HACK
     u32* data = (u32*)(rom->GetRom().data() + rom->GetTextOffset());
@@ -163,13 +83,6 @@ int main(int argc, const char* argv[]) {
     Hydra::Hypervisor::Hypervisor hypervisor(horizon);
     hypervisor.LoadROM(rom);
     hypervisor.Run();
-
-    /*
-    std::vector<u32> print_code(rom->GetText().size() / sizeof(u32));
-    std::memcpy(print_code.data(), rom->GetText().data(),
-                rom->GetText().size());
-    PrintCodeAsCArray(print_code, 24);
-    */
 
     return 0;
 }
