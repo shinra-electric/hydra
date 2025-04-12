8b4d3686d8c247fe1233e45d5a41ea730885d623
Samuliak <samuliak77@gmail.com>
2025-04-12 13:33:35 +0200

    [dynarmic] set stack pointer

--- src/core/hw/tegra_x1/cpu/dynarmic/thread.cpp
+++ src/core/hw/tegra_x1/cpu/dynarmic/thread.cpp
@@ -20,7 +20,7 @@ void Thread::Configure(const std::function<bool(ThreadBase*, u64)>&
                        uptr tls_mem_base /*,
   uptr rom_mem_base*/, uptr stack_mem_end) {
     svc_handler = svc_handler_;
-    tpidrro_el0 = mmu->UnmapAddr(tls_mem_base);
+    tpidrro_el0 = tls_mem_base;
 
     // TODO: do this in the costructor
 
@@ -50,6 +50,8 @@ void Thread::Configure(const std::function<bool(ThreadBase*, u64)>&
     config.code_cache_size = 128 * 1024 * 1024; // 128_MiB;
 
     jit = new DynA64::Jit(config);
+
+    jit->SetSP(stack_mem_end);
 }
 
 void Thread::Run() { jit->Run(); }
@@ -59,42 +61,44 @@ void Thread::LogRegisters(bool simd, u32 count) {
 }
 
 u8 Thread::MemoryRead8(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read U8 0x{:08x}", addr);
     return mmu->Load<u8>(addr);
 }
 
 u16 Thread::MemoryRead16(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read U16 0x{:08x}", addr);
     return mmu->Load<u16>(addr);
 }
 
 u32 Thread::MemoryRead32(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read U32 0x{:08x}", addr);
     return mmu->Load<u32>(addr);
 }
 
 u64 Thread::MemoryRead64(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read U64 0x{:08x}", addr);
     return mmu->Load<u64>(addr);
 }
 
 DynA64::Vector Thread::MemoryRead128(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read U128 0x{:08x}", addr);
     return mmu->Load<DynA64::Vector>(addr);
 }
 
 std::optional<u32> Thread::MemoryReadCode(u64 addr) {
-    LOG_DEBUG(Dynarmic, "Read code 0x{:08x}", addr);
     return mmu->Load<u32>(addr);
 }
 
-void Thread::MemoryWrite8(u64 addr, u8 value) { mmu->Store(addr, value); }
+void Thread::MemoryWrite8(u64 addr, u8 value) {
+    mmu->Store(addr, value);
+}
 
-void Thread::MemoryWrite16(u64 addr, u16 value) { mmu->Store(addr, value); }
+void Thread::MemoryWrite16(u64 addr, u16 value) {
+    mmu->Store(addr, value);
+}
 
-void Thread::MemoryWrite32(u64 addr, u32 value) { mmu->Store(addr, value); }
+void Thread::MemoryWrite32(u64 addr, u32 value) {
+    mmu->Store(addr, value);
+}
 
-void Thread::MemoryWrite64(u64 addr, u64 value) { mmu->Store(addr, value); }
+void Thread::MemoryWrite64(u64 addr, u64 value) {
+    mmu->Store(addr, value);
+}
 
 void Thread::MemoryWrite128(u64 addr, DynA64::Vector value) {
     mmu->Store(addr, value);
