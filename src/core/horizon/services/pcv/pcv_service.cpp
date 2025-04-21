5179969d2f4da9d7f5154c1e780342ff9f51233c
Samuliak <samuliak77@gmail.com>
2025-04-21 19:52:19 +0200

    [services::pcv] implement clock rate setting

--- src/core/horizon/services/pcv/pcv_service.cpp
+++ src/core/horizon/services/pcv/pcv_service.cpp
@@ -1,106 +1,5 @@
 #include "core/horizon/services/pcv/pcv_service.hpp"
 
-#include "core/hw/tegra_x1/cpu/const.hpp"
-
-namespace Hydra::Horizon::Services::Pcv {
-
-namespace {
-
-enum class ModuleId : u32 {
-    Cpu = 0,
-    Gpu = 1,
-    I2s1 = 2,
-    I2s2 = 3,
-    I2s3 = 4,
-    Pwm = 5,
-    I2c1 = 6,
-    I2c2 = 7,
-    I2c3 = 8,
-    I2c4 = 9,
-    I2c5 = 10,
-    I2c6 = 11,
-    Spi1 = 12,
-    Spi2 = 13,
-    Spi3 = 14,
-    Spi4 = 15,
-    Disp1 = 16,
-    Disp2 = 17,
-    Isp = 18,
-    Vi = 19,
-    Sdmmc1 = 20,
-    Sdmmc2 = 21,
-    Sdmmc3 = 22,
-    Sdmmc4 = 23,
-    Owr = 24,
-    Csite = 25,
-    Tsec = 26,
-    Mselect = 27,
-    Hda2codec2x = 28,
-    Actmon = 29,
-    I2cSlow = 30,
-    Sor1 = 31,
-    Sata = 32,
-    Hda = 33,
-    XusbCoreHostSrc = 34,
-    XusbFalconSrc = 35,
-    XusbFsSrc = 36,
-    XusbCoreDevSrc = 37,
-    XusbSsSrc = 38,
-    UartA = 39,
-    UartB = 40,
-    UartC = 41,
-    UartD = 42,
-    Host1x = 43,
-    Entropy = 44,
-    SocTherm = 45,
-    Vic = 46,
-    Nvenc = 47,
-    Nvjpg = 48,
-    Nvdec = 49,
-    Qspi = 50,
-    ViI2c = 51,
-    Tsecb = 52,
-    Ape = 53,
-    AudioDsp = 54,
-    AudioUart = 55,
-    Emc = 56,
-    Plle = 57,
-    PlleHwSeq = 58,
-    Dsi = 59,
-    Maud = 60,
-    Dpaux1 = 61,
-    MipiCal = 62,
-    UartFstMipiCal = 63,
-    Osc = 64,
-    SysBus = 65,
-    SorSafe = 66,
-    XusbSs = 67,
-    XusbHost = 68,
-    XusbDevice = 69,
-    Extperiph1 = 70,
-    Ahub = 71,
-    Hda2hdmicodec = 72,
-    Gpuaux = 73,
-    UsbD = 74,
-    Usb2 = 75,
-    Pcie = 76,
-    Afi = 77,
-    PciExClk = 78,
-    PExUsbPhy = 79,
-    XUsbPadCtl = 80,
-    Apbdma = 81,
-    Usb2TrkClk = 82,
-    XUsbIoPll = 83,
-    XUsbIoPllHwSeq = 84,
-    Cec = 85,
-    Extperiph2 = 86,
-    OscClk = invalid<u32>(), // TODO
-};
-
-}
-
-} // namespace Hydra::Horizon::Services::Pcv
-
 ENABLE_ENUM_FORMATTING(
     Hydra::Horizon::Services::Pcv::ModuleId, Cpu, "CPU", Gpu, "GPU", I2s1,
     "I2S1", I2s2, "I2S2", I2s3, "I2S3", Pwm, "PWM", I2c1, "I2C1", I2c2, "I2C2",
@@ -129,20 +28,29 @@ ENABLE_ENUM_FORMATTING(
 
 namespace Hydra::Horizon::Services::Pcv {
 
-DEFINE_SERVICE_COMMAND_TABLE(IPcvService, 3, GetClockRate)
+DEFINE_SERVICE_COMMAND_TABLE(IPcvService, 2, SetClockRate, 3, GetClockRate)
+
+void IPcvService::SetClockRate(REQUEST_COMMAND_PARAMS) {
+    const auto module_id = readers.reader.Read<ModuleId>();
+    if (module_id >= ModuleId::Count) {
+        LOG_ERROR(HorizonServices, "Invalid module ID {}", (u32)module_id);
+        result = MAKE_KERNEL_RESULT(Kernel::Error::InvalidEnumValue);
+        return;
+    }
+
+    const auto rate = readers.reader.Read<u32>();
+    clock_rates[(u32)module_id] = rate;
+}
 
 void IPcvService::GetClockRate(REQUEST_COMMAND_PARAMS) {
     const auto module_id = readers.reader.Read<ModuleId>();
-
-    u32 clock_rate = 0;
-    switch (module_id) {
-    case ModuleId::Cpu:
-        clock_rate = HW::TegraX1::CPU::CLOCK_RATE_HZ;
-        break;
-    default:
-        LOG_NOT_IMPLEMENTED(HorizonServices, "Clock rate for {}", module_id);
-        break;
+    if (module_id >= ModuleId::Count) {
+        LOG_ERROR(HorizonServices, "Invalid module ID {}", (u32)module_id);
+        result = MAKE_KERNEL_RESULT(Kernel::Error::InvalidEnumValue);
+        return;
     }
+
+    writers.writer.Write(clock_rates[(u32)module_id]);
 }
 
 } // namespace Hydra::Horizon::Services::Pcv
--- src/core/horizon/services/pcv/pcv_service.hpp
+++ src/core/horizon/services/pcv/pcv_service.hpp
@@ -1,8 +1,102 @@
 #pragma once
 
 #include "core/horizon/kernel/service_base.hpp"
+#include "core/hw/tegra_x1/cpu/const.hpp"
 
 namespace Hydra::Horizon::Services::Pcv {
+
+enum class ModuleId : u32 {
+    Cpu = 0,
+    Gpu = 1,
+    I2s1 = 2,
+    I2s2 = 3,
+    I2s3 = 4,
+    Pwm = 5,
+    I2c1 = 6,
+    I2c2 = 7,
+    I2c3 = 8,
+    I2c4 = 9,
+    I2c5 = 10,
+    I2c6 = 11,
+    Spi1 = 12,
+    Spi2 = 13,
+    Spi3 = 14,
+    Spi4 = 15,
+    Disp1 = 16,
+    Disp2 = 17,
+    Isp = 18,
+    Vi = 19,
+    Sdmmc1 = 20,
+    Sdmmc2 = 21,
+    Sdmmc3 = 22,
+    Sdmmc4 = 23,
+    Owr = 24,
+    Csite = 25,
+    Tsec = 26,
+    Mselect = 27,
+    Hda2codec2x = 28,
+    Actmon = 29,
+    I2cSlow = 30,
+    Sor1 = 31,
+    Sata = 32,
+    Hda = 33,
+    XusbCoreHostSrc = 34,
+    XusbFalconSrc = 35,
+    XusbFsSrc = 36,
+    XusbCoreDevSrc = 37,
+    XusbSsSrc = 38,
+    UartA = 39,
+    UartB = 40,
+    UartC = 41,
+    UartD = 42,
+    Host1x = 43,
+    Entropy = 44,
+    SocTherm = 45,
+    Vic = 46,
+    Nvenc = 47,
+    Nvjpg = 48,
+    Nvdec = 49,
+    Qspi = 50,
+    ViI2c = 51,
+    Tsecb = 52,
+    Ape = 53,
+    AudioDsp = 54,
+    AudioUart = 55,
+    Emc = 56,
+    Plle = 57,
+    PlleHwSeq = 58,
+    Dsi = 59,
+    Maud = 60,
+    Dpaux1 = 61,
+    MipiCal = 62,
+    UartFstMipiCal = 63,
+    Osc = 64,
+    SysBus = 65,
+    SorSafe = 66,
+    XusbSs = 67,
+    XusbHost = 68,
+    XusbDevice = 69,
+    Extperiph1 = 70,
+    Ahub = 71,
+    Hda2hdmicodec = 72,
+    Gpuaux = 73,
+    UsbD = 74,
+    Usb2 = 75,
+    Pcie = 76,
+    Afi = 77,
+    PciExClk = 78,
+    PExUsbPhy = 79,
+    XUsbPadCtl = 80,
+    Apbdma = 81,
+    Usb2TrkClk = 82,
+    XUsbIoPll = 83,
+    XUsbIoPllHwSeq = 84,
+    Cec = 85,
+    Extperiph2 = 86,
+    OscClk = 87, // TODO: correct?
+
+    Count = 88,
+};
 
 class IPcvService : public Kernel::ServiceBase {
   public:
@@ -12,7 +106,12 @@ class IPcvService : public Kernel::ServiceBase {
     void RequestImpl(REQUEST_IMPL_PARAMS) override;
 
   private:
+    // TODO: other clock rates
+    u32 clock_rates[(u32)ModuleId::Count] = {HW::TegraX1::CPU::CLOCK_RATE_HZ,
+                                             0};
+
     // Commands
+    void SetClockRate(REQUEST_COMMAND_PARAMS);
     void GetClockRate(REQUEST_COMMAND_PARAMS);
 };
 
