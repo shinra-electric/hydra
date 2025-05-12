eec919bec93747ad15925d02c498167176ad5181
Samuliak <samuliak77@gmail.com>
2025-05-12 09:24:55 +0200

    horizon: implement user manager

--- src/core/horizon/user_manager.cpp
+++ src/core/horizon/user_manager.cpp
@@ -1,13 +1,75 @@
 #include "core/horizon/user_manager.hpp"
 
+#define DEFAULT_USER_NAME "Hydra user"
+
 namespace hydra::horizon {
+
+namespace {
+
+constexpr uuid_t USER_ID_START = 0x80000000;
+constexpr uuid_t INVALID_USER_ID = USER_ID_START;
+constexpr uuid_t FIRST_USER_ID = USER_ID_START + 1;
+
+}
 
 SINGLETON_DEFINE_GET_INSTANCE(UserManager, Horizon)
 
 UserManager::UserManager() {
     SINGLETON_SET_INSTANCE(UserManager, Horizon);
+
+    // TODO: deserialize all users
+
+    if (users.empty()) {
+        auto user_id = Create();
+        Serialize(user_id);
+    }
 }
 
 UserManager::~UserManager() { SINGLETON_UNSET_INSTANCE(); }
+
+uuid_t UserManager::Create() {
+    // First, find an available ID
+    uuid_t user_id = FIRST_USER_ID;
+    while (true) {
+        if (!users.contains(user_id))
+            break;
+
+        user_id++;
+    }
+
+    // Create
+    auto& user_pair = users[user_id];
+    user_pair.first = {
+        .name = DEFAULT_USER_NAME,
+    };
+    user_pair.second = true;
+
+    return user_id;
+}
+
+void UserManager::Flush() {
+    for (auto& [user_id, user_pair] : users)
+        Serialize(user_id);
+}
+
+void UserManager::Serialize(uuid_t user_id) {
+    auto& user_pair = users[user_id];
+    if (!user_pair.second)
+        return;
+
+    LOG_FUNC_NOT_IMPLEMENTED(Horizon);
+
+    user_pair.second = false;
+}
+
+void UserManager::Deserialize(uuid_t user_id) {
+    auto& user_pair = users[user_id];
+    if (user_pair.second)
+        LOG_WARN(Horizon, "Overwriting user 0x{:08x}", user_id);
+
+    LOG_FUNC_NOT_IMPLEMENTED(Horizon);
+
+    user_pair.second = false;
+}
 
 } // namespace hydra::horizon
--- src/core/horizon/user_manager.hpp
+++ src/core/horizon/user_manager.hpp
@@ -4,6 +4,11 @@
 
 namespace hydra::horizon {
 
+struct User {
+    std::string name;
+    // TODO: more
+};
+
 class UserManager {
   public:
     static UserManager& GetInstance();
@@ -11,7 +16,27 @@ class UserManager {
     UserManager();
     ~UserManager();
 
+    uuid_t Create();
+
+    void Flush();
+
+    // Getters
+    const User& Get(uuid_t user_id) const {
+        auto it = users.find(user_id);
+        ASSERT(it != users.end(), Horizon, "Invalid user 0x{:08x}", user_id);
+
+        return it->second.first;
+    }
+
+    // Setters
+    void Set(uuid_t user_id, const User& user) { users[user_id] = {user, true}; }
+
   private:
+    std::map<uuid_t, std::pair<User, bool>> users;
+
+    // Helpers
+    void Serialize(uuid_t user_id);
+    void Deserialize(uuid_t user_id);
 };
 
 } // namespace hydra::horizon
