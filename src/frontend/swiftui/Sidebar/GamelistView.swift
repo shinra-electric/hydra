5c76c5289e85febd02da67328c02c9beae118881
Samuliak <samuliak77@gmail.com>
2025-05-18 09:54:09 +0200

    swiftui: initialize emulation context

--- src/frontend/swiftui/Sidebar/GamelistView.swift
+++ src/frontend/swiftui/Sidebar/GamelistView.swift
@@ -1,29 +1,69 @@
 import SwiftUI
 
+struct GamePreview: View {
+    let game: Game
+    @State private var lastClickTime: Date? = nil
+    @State private var navigate = false
+
+    var body: some View {
+        // TODO: this type of NavigationLink usage is deprecated
+        NavigationLink(destination: EmulationView(game: game), isActive: $navigate) {
+            Text(game.path)
+                .padding()
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    if let lastTime = lastClickTime, Date().timeIntervalSince(lastTime) < 0.3 {
+                        navigate = true
+                        lastClickTime = nil
+                    } else {
+                        lastClickTime = Date()
+                    }
+                }
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
+
 struct GameListView: View {
-    @State private var game_directories: [String] = []
+    @State private var games: [Game] = []
 
     var body: some View {
         List {
-            ForEach(game_directories.indices, id: \.self) { index in
-                Text(game_directories[index])
+            ForEach(self.games.indices, id: \.self) { index in
+                GamePreview(game: games[index])
                     .padding(.vertical, 8)
             }
         }
-        //.navigationTitle("C String Array")
+        //.navigationTitle("")
         .onAppear {
-            loadGamePreviews()
+            load()
         }
     }
 
-    func loadGamePreviews() {
+    func load() {
         // Game directories
-        let game_directories_option = hydra_config_get_game_directories()
-        for i in 0..<hydra_string_array_option_get_count(game_directories_option) {
-            if let game_dir_raw = hydra_string_array_option_get(game_directories_option, UInt32(i))
-            {
-                let game_dir = String(cString: game_dir_raw)
-                game_directories.append(game_dir)
+        let gameDirsOption = hydra_config_get_game_directories()
+        for i in 0..<hydra_string_array_option_get_count(gameDirsOption) {
+            if let gameDirRaw = hydra_string_array_option_get(gameDirsOption, UInt32(i)) {
+                let gameDir = String(cString: gameDirRaw)
+
+                // Iterate recursively over the game directories
+                let fileManager = FileManager.default
+                guard let enumerator = fileManager.enumerator(atPath: gameDir) else {
+                    // TODO: error popup
+                    print("Invalid game directory \(gameDir)")
+                    return
+                }
+                while let gamePath = enumerator.nextObject() as? String {
+                    // TODO: ask the core for supported extensions
+                    if !gamePath.hasSuffix("nro") && !gamePath.hasSuffix("nso")
+                        && !gamePath.hasSuffix("nca")
+                    {
+                        continue
+                    }
+
+                    self.games.append(Game(path: gamePath))
+                }
             }
         }
     }
