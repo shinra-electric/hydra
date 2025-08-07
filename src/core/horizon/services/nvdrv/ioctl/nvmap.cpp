cd00c90b18d63fda32617aee2b7e3aca92736556
Samuliak <samuliak77@gmail.com>
2025-08-07 12:30:59 +0200

    services/nvdrv: NvMap::Param

--- src/core/horizon/services/nvdrv/ioctl/nvmap.cpp
+++ src/core/horizon/services/nvdrv/ioctl/nvmap.cpp
@@ -4,9 +4,10 @@
 
 namespace hydra::horizon::services::nvdrv::ioctl {
 
-DEFINE_IOCTL_TABLE(NvMap, DEFINE_IOCTL_TABLE_ENTRY(NvMap, 0x01, 0x01, Create,
-                                                   0x03, FromId, 0x04, Alloc,
-                                                   0x05, Free, 0x0E, GetId))
+DEFINE_IOCTL_TABLE(NvMap,
+                   DEFINE_IOCTL_TABLE_ENTRY(NvMap, 0x01, 0x01, Create, 0x03,
+                                            FromId, 0x04, Alloc, 0x05, Free,
+                                            0x09, Param, 0x0E, GetId))
 
 NvResult NvMap::Create(u32 size, handle_id_t* out_handle_id) {
     *out_handle_id = GPU_INSTANCE.CreateMap(size);
@@ -22,6 +23,7 @@ NvResult NvMap::Alloc(handle_id_t handle_id, u32 heap_mask, u32 flags,
                       InOutSingle<u32> inout_alignment, aligned<u8, 8> kind,
                       gpu_vaddr_t addr) {
     // TODO: flags wtf
+    // TODO: kind
     GPU_INSTANCE.AllocateMap(handle_id, addr, flags == 1);
     inout_alignment = hw::tegra_x1::gpu::GPU_PAGE_SIZE; // TODO: correct?
     return NvResult::Success;
@@ -35,6 +37,36 @@ NvResult NvMap::Free(aligned<handle_id_t, 8> handle_id, gpu_vaddr_t* out_addr,
     *out_addr = map.addr;
     *out_size = map.size;
     *out_flags = map.write ? 1 : 0; // TODO: correct?
+    return NvResult::Success;
+}
+
+NvResult NvMap::Param(handle_id_t handle_id, NvMapParamType type,
+                      u32* out_value) {
+    auto map = GPU_INSTANCE.GetMap(handle_id);
+    switch (type) {
+    case NvMapParamType::Size:
+        *out_value = map.size;
+        break;
+    case NvMapParamType::Alignment:
+        *out_value = hw::tegra_x1::gpu::GPU_PAGE_SIZE; // TODO: correct?
+        break;
+    case NvMapParamType::Heap:
+        *out_value = 0x40000000;
+        break;
+    case NvMapParamType::Kind:
+        LOG_NOT_IMPLEMENTED(Services, "NvMapParamType::Kind");
+        // TODO
+        //*out_value = map.kind;
+        *out_value = 0;
+        break;
+    case NvMapParamType::Compr:
+        LOG_NOT_IMPLEMENTED(Services, "NvMapParamType::Compr");
+        *out_value = 0;
+        break;
+    default: // Also includes Base
+        // TODO: return NvInternalResult::InvalidInput?
+        return NvResult::NotSupported;
+    }
     return NvResult::Success;
 }
 
--- src/core/horizon/services/nvdrv/ioctl/nvmap.hpp
+++ src/core/horizon/services/nvdrv/ioctl/nvmap.hpp
@@ -4,6 +4,15 @@
 
 namespace hydra::horizon::services::nvdrv::ioctl {
 
+enum class NvMapParamType : u32 {
+    Size = 1,
+    Alignment = 2,
+    Base = 3, // Returns error
+    Heap = 4, // Always 0x40000000
+    Kind = 5,
+    Compr = 6, // Unused
+};
+
 class NvMap : public FdBase {
   public:
     NvResult Ioctl(IoctlContext& context, u32 type, u32 nr) override;
@@ -17,6 +26,7 @@ class NvMap : public FdBase {
                    gpu_vaddr_t addr);
     NvResult Free(aligned<handle_id_t, 8> handle_id, gpu_vaddr_t* out_addr,
                   u64* out_size, u32* out_flags);
+    NvResult Param(handle_id_t handle_id, NvMapParamType type, u32* out_value);
     NvResult GetId(u32* out_id, handle_id_t handle_id);
 };
 
