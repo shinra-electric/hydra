de402d8bd87852aeaf473ba3c9f04b1bf6c6805f
Samuliak <samuliak77@gmail.com>
2025-08-27 23:01:46 +0200

    display: lock mutex in GetAccumulatedDTForMainLayer

--- src/core/horizon/display/display.cpp
+++ src/core/horizon/display/display.cpp
@@ -33,6 +33,8 @@ void Display::Present(u32 width, u32 height) {
 }
 
 AccumulatedTime Display::GetAccumulatedDTForMainLayer() {
+    std::lock_guard lock(mutex);
+
     // TODO: get the main layer for the main process
     const auto layer_id = 1;
     if (!layer_pool.IsValid(layer_id))
