26f162f88fafdd788051e68d733c9cc66601dfd6
Samuliak <samuliak77@gmail.com>
2025-08-28 10:37:34 +0200

    display: treat the first valid layer as the main layer

--- src/core/horizon/display/display.cpp
+++ src/core/horizon/display/display.cpp
@@ -34,12 +34,13 @@ void Display::Present(u32 width, u32 height) {
 
 Layer* Display::GetMainLayer() {
     std::lock_guard lock(mutex);
-    // TODO: get the main layer for the main process
-    const auto layer_id = 1;
-    if (!layer_pool.IsValid(layer_id))
-        return nullptr;
+    for (u32 layer_id = 1; layer_id < layer_pool.GetCapacity() + 1;
+         layer_id++) {
+        if (layer_pool.IsValid(layer_id))
+            return layer_pool.Get(layer_id);
+    }
 
-    return layer_pool.Get(layer_id);
+    return nullptr;
 }
 
 } // namespace hydra::horizon::display
