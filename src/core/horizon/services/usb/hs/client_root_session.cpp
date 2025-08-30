1e5731a3a35e4c83b2ca09ae1d64d8ff92ba0086
Samuliak <samuliak77@gmail.com>
2025-08-30 21:07:59 +0200

    services/usb/hs: stub

--- src/core/horizon/services/usb/hs/client_root_session.cpp
+++ src/core/horizon/services/usb/hs/client_root_session.cpp
@@ -1,7 +1,38 @@
 #include "core/horizon/services/usb/hs/client_root_session.hpp"
 
+#include "core/horizon/kernel/event.hpp"
+#include "core/horizon/kernel/process.hpp"
+
 namespace hydra::horizon::services::usb::hs {
 
-DEFINE_SERVICE_COMMAND_TABLE(IClientRootSession)
+DEFINE_SERVICE_COMMAND_TABLE(IClientRootSession, 0, BindClientProcess, 4,
+                             CreateInterfaceAvailableEvent, 6,
+                             GetInterfaceStateChangeEvent)
+
+IClientRootSession::IClientRootSession()
+    : interface_available_event{new kernel::Event()},
+      interface_state_change_event{new kernel::Event()} {}
+
+result_t IClientRootSession::BindClientProcess() {
+    LOG_FUNC_STUBBED(Services);
+    return RESULT_SUCCESS;
+}
+
+result_t IClientRootSession::CreateInterfaceAvailableEvent(
+    kernel::Process* process, aligned<u8, 2> index, DeviceFilter device_filter,
+    OutHandle<HandleAttr::Copy> out_handle) {
+    LOG_FUNC_STUBBED(Services);
+
+    out_handle = process->AddHandle(interface_available_event);
+    return RESULT_SUCCESS;
+}
+
+result_t IClientRootSession::GetInterfaceStateChangeEvent(
+    kernel::Process* process, OutHandle<HandleAttr::Copy> out_handle) {
+    LOG_FUNC_STUBBED(Services);
+
+    out_handle = process->AddHandle(interface_state_change_event);
+    return RESULT_SUCCESS;
+}
 
 } // namespace hydra::horizon::services::usb::hs
--- src/core/horizon/services/usb/hs/client_root_session.hpp
+++ src/core/horizon/services/usb/hs/client_root_session.hpp
@@ -2,14 +2,46 @@
 
 #include "core/horizon/services/const.hpp"
 
+namespace hydra::horizon::kernel {
+class Event;
+}
+
 namespace hydra::horizon::services::usb::hs {
 
+struct DeviceFilter {
+    u16 flags;
+    u16 vendor_id;
+    u16 product_id;
+    u16 bcd_device_min;
+    u16 bcd_device_max;
+    u8 b_device_class;
+    u8 b_device_subclass;
+    u8 b_device_protocol;
+    u8 b_interface_class;
+    u8 b_interface_subclass;
+    u8 b_interface_protocol;
+};
+
+// NOTE: The commands are completely different on 1.0.0
 class IClientRootSession : public IService {
+  public:
+    IClientRootSession();
+
   protected:
     result_t RequestImpl(RequestContext& context, u32 id) override;
 
   private:
+    kernel::Event* interface_available_event;
+    kernel::Event* interface_state_change_event;
+
     // Commands
+    result_t BindClientProcess(); // 2.0.0+
+    result_t CreateInterfaceAvailableEvent(
+        kernel::Process* process, aligned<u8, 2> index,
+        DeviceFilter device_filter, OutHandle<HandleAttr::Copy> out_handle);
+    result_t
+    GetInterfaceStateChangeEvent(kernel::Process* process,
+                                 OutHandle<HandleAttr::Copy> out_handle);
 };
 
 } // namespace hydra::horizon::services::usb::hs
