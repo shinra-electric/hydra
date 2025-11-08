390b93de9780731e25d700ecb6c6e1c1b117101a
Samuel Žúbor <samuliak77@gmail.com>
2025-11-08 19:19:55 +0100

    services/ro: IRoInterface::MapManualLoadModuleMemory

--- src/core/horizon/services/ro/detail/ro_interface.cpp
+++ src/core/horizon/services/ro/detail/ro_interface.cpp
@@ -12,7 +12,13 @@ result_t IRoInterface::MapManualLoadModuleMemory(kernel::Process* process,
                                                  u64 pid_reserved, u64 nro_addr,
                                                  u64 nro_size, u64 bss_addr,
                                                  u64 bss_size, u64* out_addr) {
-    LOG_FUNC_NOT_IMPLEMENTED(Services);
+    auto mmu = process->GetMmu();
+    const auto base =
+        mmu->FindFreeMemory(kernel::EXECUTABLE_REGION, nro_size + bss_size);
+    mmu->Map(base, nro_addr, nro_size);
+    mmu->Map(base + nro_size, bss_addr, bss_size);
+
+    *out_addr = base;
     return RESULT_SUCCESS;
 }
 
