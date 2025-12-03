109f07e29f6cec716a81fb04aa6e99734089d286
shinra-electric <50119606+shinra-electric@users.noreply.github.com>
2025-12-03 17:06:26 +0900

    Merge play/pause buttons (#82)

--- src/frontend/swiftui/EmulationToolbarItems.swift
+++ src/frontend/swiftui/EmulationToolbarItems.swift
@@ -8,17 +8,21 @@ struct EmulationToolbarItems: ToolbarContent {
     var body: some ToolbarContent {
         #if os(macOS)
             ToolbarItemGroup(placement: .principal) {
-                Button("Resume", systemImage: "play") {
-                    guard let emulationContext = emulationState.emulationContext else { return }
-                    emulationContext.resume()
-                    isRunning = true
-                }.disabled(isRunning)
-                Button("Pause", systemImage: "pause") {
-                    guard let emulationContext = emulationState.emulationContext else { return }
-                    emulationContext.pause()
-                    isRunning = false
-                }.disabled(!isRunning)
-                Button("Stop", systemImage: "stop") {
+                if isRunning {
+                    Button("Pause", systemImage: "pause.fill") {
+                        guard let emulationContext = emulationState.emulationContext else { return }
+                        emulationContext.pause()
+                        isRunning = false
+                    }
+                } else {
+                    Button("Resume", systemImage: "play.fill") {
+                        guard let emulationContext = emulationState.emulationContext else { return }
+                        emulationContext.resume()
+                        isRunning = true
+                    }
+                }
+                
+                Button("Stop", systemImage: "stop.fill") {
                     guard let emulationContext = emulationState.emulationContext else { return }
                     emulationContext.requestStop()
                     // TODO: wait a bit?
